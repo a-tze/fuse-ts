de1f6a36344d2e9082e24e32dd9a3bbf6fd45d95
atze <atze@fem.tu-ilmenau.de>
2015-12-04 21:38:19 +0100

    remove fuse-vdv, make repo suitable for automatic building

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-fuse-video
-==========
+fuse-ts
+=======
 
-These two tools, fuse-TS and fuse-DV, provide FUSE based filesystems that 
-enable you to virtually concatenate a sequence of files. Given the purpose
-of the tools, they work best with video files of course. 
+This tool provides a FUSE based filesystems that enables you to virtually 
+concatenate a sequence of files. Given the purpose of the tool, it works 
+best with video files.
 
-The tools are tuned for the needs of the FeM/VOC CRS project (URL to come 
+The tool is tuned for the needs of the FeM/VOC CRS project (URL to come 
 once it's publicly released) which contains some more documentation.
 
--- fuse-vdv/fuse-vdv-shotcut.h
+++
@@ -1,9 +0,0 @@
-extern size_t get_shotcut_project_file_size (const char * filename, int num_frames, int blanklen);
-extern void init_shotcut_project_file ();
-extern size_t shotcut_read(const char *path, char *buf, size_t size, off_t offset, const char* movie_path, int frames, int blanklen);
-extern void open_shotcut_project_file (const char* movie_path, int frames, int blanklen, int truncate);
-extern void truncate_shotcut_project_file();
-extern size_t write_shotcut_project_file (const char * buffer, size_t size, off_t offset);
-extern void close_shotcut_project_file ();
-extern int find_cutmarks_in_shotcut_project_file(int* inframe, int* outframe, int* blanklen);
-extern char * shotcut_path;
